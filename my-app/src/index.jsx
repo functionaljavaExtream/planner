584515a004d5655bd47f1f5b8b8db9d464ff19ed
KJP <mapodaegyo12@gmail.com>
2021-08-04 23:29:00 +0900

    js -> jsx

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "Ki"
+const ele =   <h1>Hello, {name}</h1>;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  ele,
   document.getElementById('root')
+  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
