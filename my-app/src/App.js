74ed2cdc44b136834989fe8acea8b06864d1e775
KJP <mapodaegyo12@gmail.com>
2021-08-04 23:13:11 +0900

    commit todolist

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> this is first
+          Edit <code>src/App.js</code> this is todoList
         </p>
         <a
           className="App-link"
